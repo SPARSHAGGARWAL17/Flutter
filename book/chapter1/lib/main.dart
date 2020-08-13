4ddced95bcc0fd74fe205adfaca5af4d6a97f74d
Sparsh Aggarwal <aggarwalsparsh175@gmail.com>
2020-08-13 23:07:45 +0530

    Chapter 7

--- book/chapter1/lib/main.dart
+++ book/chapter1/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-bool dark = true;
 void main() {
   runApp(MyApp());
 }
@@ -10,20 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Flutter Demo',
-      darkTheme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color: Colors.grey[700],
-          brightness: Brightness.dark,
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        canvasColor: Colors.deepOrangeAccent[100],
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      theme: dark
-          ? ThemeData(
-              canvasColor: Colors.deepOrange[100],
-              primarySwatch: Colors.deepOrange,
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            )
-          : ThemeData.dark(),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -40,11 +32,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool showFirst = true;
+  double height = 100;
+  double width = 100;
+  _increase() {
+    setState(() {
+      width = width >= 200 ? 100 : width += 50;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
-      dark = true;
       _counter++;
+    });
+  }
+
+  void _crossFade() {
+    setState(() {
+      showFirst = showFirst ? false : true;
     });
   }
 
@@ -52,15 +57,60 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.menu),
         title: Text(widget.title),
+        bottom: PreferredSize(child: Container(), preferredSize: null),
+        actions: [
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Icon(
+            Icons.more_vert,
+            color: Colors.white,
+          )
+        ],
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text('You have pushed the button this many times'),
+            FlatButton(
+              onPressed: _increase,
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 100),
+                height: height,
+                width: width,
+                color: Colors.purple,
+              ),
+            ),
+            AnimatedCrossFade(
+              firstChild: Container(
+                height: 100,
+                width: 200,
+                color: Colors.purple[900],
+              ),
+              secondChild: Container(
+                height: 100,
+                width: 400,
+                color: Colors.purple[900],
+              ),
+              crossFadeState: showFirst
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: Duration(milliseconds: 200),
+            ),
+            Positioned.fill(
+                child: FlatButton(onPressed: _crossFade, child: Text('Press'))),
+            Text(
+              'You have pushed the button this many times:',
+            ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
@@ -70,19 +120,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class Theme extends StatelessWidget {
-  const Theme({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'You have pushed the button this many times:',
     );
   }
 }
