6abb22d5b9789a7301c8a12fc8273805772bb625
Sparsh Aggarwal <aggarwalsparsh175@gmail.com>
2020-08-14 20:40:46 +0530

    animation

--- book/chapter1/lib/main.dart
+++ book/chapter1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,8 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        canvasColor: Colors.deepOrangeAccent[100],
-        primarySwatch: Colors.deepOrange,
+        canvasColor: Colors.black,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,95 +31,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   bool showFirst = true;
-  double height = 100;
+
+  double height = 80;
   double width = 100;
+
   _increase() {
     setState(() {
-      width = width >= 200 ? 100 : width += 50;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _crossFade() {
-    setState(() {
-      showFirst = showFirst ? false : true;
+      height = height == 80 ? 300 : 80;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        title: Text(widget.title),
-        bottom: PreferredSize(child: Container(), preferredSize: null),
-        actions: [
-          Icon(
-            Icons.search,
-            color: Colors.white,
-          ),
-          SizedBox(
-            width: 20,
-          ),
-          Icon(
-            Icons.more_vert,
-            color: Colors.white,
-          )
-        ],
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            FlatButton(
-              onPressed: _increase,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
               child: AnimatedContainer(
-                duration: Duration(milliseconds: 100),
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                      onPressed: _increase,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: AnimatedDefaultTextStyle(
+                          child: Text('Share Files'),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: height == 80 ? 20 : 30),
+                          duration: Duration(milliseconds: 600),
+                          curve: Curves.easeInOutExpo,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                duration: Duration(milliseconds: 600),
+                curve: Curves.easeInOutExpo,
                 height: height,
-                width: width,
-                color: Colors.purple,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(25),
+                ),
               ),
-            ),
-            AnimatedCrossFade(
-              firstChild: Container(
-                height: 100,
-                width: 200,
-                color: Colors.purple[900],
-              ),
-              secondChild: Container(
-                height: 100,
-                width: 400,
-                color: Colors.purple[900],
-              ),
-              crossFadeState: showFirst
-                  ? CrossFadeState.showFirst
-                  : CrossFadeState.showSecond,
-              duration: Duration(milliseconds: 200),
-            ),
-            Positioned.fill(
-                child: FlatButton(onPressed: _crossFade, child: Text('Press'))),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
