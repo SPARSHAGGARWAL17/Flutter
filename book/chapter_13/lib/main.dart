91ee75b7ac5e04f5de1a8f7525c65002dbea03cf
Sparsh Aggarwal <aggarwalsparsh175@gmail.com>
2020-08-23 22:28:26 +0530

    database added

--- book/chapter_13/lib/main.dart
+++ book/chapter_13/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -23,6 +24,17 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
+
+class DatabaseFileRoutines {
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
@@ -33,12 +45,76 @@ class _LandingPageState extends State<LandingPage> {
     return File('$path/local_file.json');
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Text('Hello'),
-      ),
-    );
+  Future<String> readJournals() async {
+    try {
+      final file = await _localFile;
+      if (!file.existsSync()) {
+        print('File does not exists : ${file.absolute}');
+        await writeJournals('{"journals" : []}');
+      }
+      String contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      print("error journals : $e");
+      return "";
+    }
   }
+
+  Future<File> writeJournals(String json) async {
+    final File file = await _localFile;
+    return file.writeAsString('$json');
+  }
+
+  Database databaseFromJson(String str) {
+    final dataFromJson = jsonDecode(str);
+    return Database.fromJson(dataFromJson);
+  }
+
+  String databaseToJson(Database data) {
+    final dataToJson = data.toJson();
+    return jsonEncode(dataToJson);
+  }
+}
+
+class Database {
+  List<Journal> journal;
+  Database({
+    this.journal,
+  });
+
+  factory Database.fromJson(Map<String, dynamic> json) => Database(
+        journal: List<Journal>.from(
+          json["journals"].map((x) => Journal.fromJson(x)),
+        ),
+      );
+  Map<String, dynamic> toJson() => {
+        "journals": List<dynamic>.from(
+          journal.map((x) => x.toJson()),
+        ),
+      };
+}
+
+class Journal {
+  String id;
+  String date;
+  String mood;
+  String note;
+  Journal({
+    this.id,
+    this.date,
+    this.mood,
+    this.note,
+  });
+  factory Journal.fromJson(Map<String, dynamic> json) => Journal(
+        id: json["id"],
+        date: json["date"],
+        mood: json["mood"],
+        note: json["note"],
+      );
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "date": date,
+        "mood": mood,
+        "note": note,
+      };
 }
